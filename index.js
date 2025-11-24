07f238a8d079071b18e3db259e2a37f55a148c6d
reyyardell011-bit <reyyardell011@gmail.com>
2025-11-24 14:23:57 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
+const {
+  default: makeWASocket,
+  useMultiFileAuthState,
+  DisconnectReason,
+} = require("@whiskeysockets/baileys");
+const pino = require("pino");
+
+require("./keep_alive"); // Hapus kalau di Railway
+
+async function startBot() {
+  const { state, saveCreds } = await useMultiFileAuthState('./session');
+  const sock = makeWASocket({
+    logger: pino({ level: "silent" }),
+    printQRInTerminal: true,
+    auth: state,
+  });
+
+  sock.ev.on("creds.update", saveCreds);
+
+  sock.ev.on("messages.upsert", async ({ messages }) => {
+    const msg = messages[0];
+    if (!msg.message || msg.key.fromMe) return;
+
+    const chat = msg.key.remoteJid;
+    const text = msg.message.conversation || msg.message.extendedTextMessage?.text;
+
+    if (text === "ping") {
+      await sock.sendMessage(chat, { text: `pong!` });
+    }
+
+    if (text === "!menu") {
+      await sock.sendMessage(chat, { text: "MENU BOT\n1. .ping\n2. .help" });
+    }
+  });
+}
+
+startBot();
 
